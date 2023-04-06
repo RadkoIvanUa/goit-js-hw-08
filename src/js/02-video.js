ae42fba1103dff818d6ade0532bd6304530f630c
Ivan Radko <mr.radkoivan@gmail.com>
2023-04-06 16:30:57 +0300

    task03

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const TIME_KEY = 'videoplayer-current-time';
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(data) {
+  localStorage.setItem(TIME_KEY, JSON.stringify(data));
+}
+
+const seconds = JSON.parse(localStorage.getItem(TIME_KEY)).seconds;
+
+player.setCurrentTime(seconds);
